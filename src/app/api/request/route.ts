b817077f0122bb3f20a1934b9992b73fc2a2234f
ezraexe <ezrakimcs@gmail.com>
2026-01-18 16:14:24 -0500

    updated get request and new patch method

--- src/app/api/request/route.ts
+++ src/app/api/request/route.ts
@@ -1,11 +1,11 @@
 // PUT /api/request/route.ts
 
-// import { NextResponse } from "next/server"; 
 import { ResponseType } from "@/lib/types/apiResponse"; 
 import { ServerResponseBuilder } from "@/lib/builders/serverResponseBuilder"; 
 import clientPromise from "@/lib/mongodb";
 import { RequestStatus } from "@/lib/types/request";
 import { PAGINATION_PAGE_SIZE } from "@/lib/constants/config";
+import { ObjectId } from "mongodb";
 
 
 export async function PUT(request: Request) { 
@@ -54,12 +54,19 @@ export async function GET(request: Request) {
   try {
     const url = new URL(request.url);
     const page = parseInt(url.searchParams.get("page") || "1"); 
+    const status = url.searchParams.get("status"); 
+  
   
     const client = await clientPromise; 
     const db = client.db("bog-takehome");
     const collection = db.collection("requests"); 
+
+    const queryFilter: { status?: string } = {}; 
+    if (status) {
+      queryFilter.status = status; 
+    }
   
-    const allRequests = await collection.find({}).sort({ createdDate: -1 }).toArray(); 
+    const allRequests = await collection.find(queryFilter).sort({ createdDate: -1 }).toArray(); 
     const startindex = (page - 1) * PAGINATION_PAGE_SIZE;
     const paginationRequests = allRequests.slice(startindex, startindex + PAGINATION_PAGE_SIZE); 
 
@@ -72,5 +79,54 @@ export async function GET(request: Request) {
     console.error(e); 
     return new ServerResponseBuilder(ResponseType.UNKNOWN_ERROR).build(); 
   }
+}
 
+export async function PATCH(request: Request) {
+  try {
+    const req = await request.json(); 
+    const { id, status } = req; 
+
+    if (!id) {
+      return new ServerResponseBuilder(ResponseType.INVALID_INPUT).build(); 
+    }
+
+    const existingStatus = ["approved", "completed", "rejected", "pending"]; 
+    if (!status || !existingStatus.includes(status)) { 
+      return new ServerResponseBuilder(ResponseType.INVALID_INPUT).build(); 
+    }
+
+    const client = await clientPromise; 
+    const db = client.db("bog-takehome");
+    const collection = db.collection("requests");
+
+    const res = await collection.findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          status : status, 
+          lastEditedDate: new Date()
+        }
+      }, 
+      { returnDocument: "after" }
+    );
+
+    if (!res) {
+      return new ServerResponseBuilder(ResponseType.INVALID_INPUT).build();
+    }
+
+    return new Response(JSON.stringify({
+      id: res._id,
+      requestorName: res.requestorName, 
+      itemRequested: res.itemRequested, 
+      createdDate: res.createdDate, 
+      lastEditedDate: res.lastEditedDate, 
+      status: res.status, 
+    }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" }, 
+    });
+  } catch (e) {
+    console.error(e);
+    return new ServerResponseBuilder(ResponseType.UNKNOWN_ERROR).build(); 
+  }
 }
