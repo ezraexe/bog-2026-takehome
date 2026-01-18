d6929d3399b1162a50d236d407b417fa6c9ce7cd
ezraexe <ezrakimcs@gmail.com>
2026-01-17 23:04:51 -0500

    added kewl page heh

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -7,7 +7,24 @@ export default function Kewl() {
       Feel free to put links, images, whatever! 
       Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
       */}
-      😎😎😎
+      <div className="w-1/2 text-center"> 
+        Hello! My name is Ezra, and I'm a rising 4th year Computer Science major. 
+        I was previously a part of GT Esports, where I was a coordinator and played for GT Valorant during my freshman year, 
+        where we were flown out by Red Bull and played at the Georgia World Congress Center. 
+        We also ran events such as GT Game Fest, Georgia Tech's BYOC LAN event. 
+      </div>
+
+      <div> 
+        Some of my hobbies are hanging out with friends, traveling, taking pictures, and going to the gym. 
+      </div>
+
+      <div className="w-1/2 text-center"> 
+        Something "special" that happened to me last year was that I broke my arm while arm wrestling at my friend's graduation party.
+        I've been trying to rehab and build the strength back up since... 
+      </div>
+
+
+      
     </div>
   );
 }
